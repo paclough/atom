febadb433a20d89f36ce594f39ecd046c25d4ec6
Paul Clough <pclough@miami.edu>
2025-10-22 11:50:25 -0400

    Add UserReview input and comments

--- plugins/umlRequestMaterialPlugin/modules/umlRequestMaterial/actions/requestAction.class.php
+++ plugins/umlRequestMaterialPlugin/modules/umlRequestMaterial/actions/requestAction.class.php
@@ -2,8 +2,10 @@
 
 class RequestMaterialAction {
 	public static function getRequestLink( $resource ) {
+        // Collect metadata
         $collectionIdentifier = $resource->getCollectionRoot()->identifier;
         $repositoryCode       = substr($collectionIdentifier, 0, 3);
+        // Assign 'ASM' and 'ASU' repository codes to 'ASC' (Special Collections)
         if ( $repositoryCode === 'ASM' || $repositoryCode == 'ASU' ) { $repositoryCode = 'ASC'; }
 
         $id                   = explode( ":", $resource->getCollectionRoot()->descriptionIdentifier )[ 1 ];
@@ -38,6 +40,7 @@ class RequestMaterialAction {
         }
         $location = $breadcrumb . ">" . $location;
 
+        // For ARC material create a form to submit a Primo request
         if ( $repository == "Architecture Research Center, University of Miami Libraries" ):
           $request = <<<EOT
 <form name="PrimoRequest" target="_blank" method="post" action="https://miami.primo.exlibrisgroup.com/discovery/search?query=holding_call_number,contains,$collectionIdentifier&vid=01UOML_INST:uml_new" style="display: inline">
@@ -45,6 +48,9 @@ class RequestMaterialAction {
 </form>
 EOT;
         else:
+        // For all other collections create a form to submit an Aeon request
+        // For testing, use the UserReview input so the request isn't delivered via Aeon immediately
+        // For production, remove the UserReview input or use an HTML comment
           $request = <<<EOT
 <form name='AeonRequest' target='_blank' method = 'post' action='https://aeon.library.miami.edu/aeon/aeon.dll' style='display: inline'>
 <input name='AeonForm' value='EADRequest' type='hidden'>
@@ -60,6 +66,7 @@ EOT;
 <input value="$repository" name="Location_$id" type="hidden">
 <input value="$repositoryCode" name="Site" type="hidden">
 <input value="$collectionIdentifier" name="CallNumber_$id" type="hidden">
+<!-- <input id="UserReview" name="UserReview" value="Yes" type="checkbox" style="display:none" checked="checked"> -->
 </div>
 
 <input name='SubmitButton' value='Submit request' type='submit' class='btn-request-material'>
