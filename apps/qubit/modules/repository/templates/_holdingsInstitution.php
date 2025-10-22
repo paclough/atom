ebe9980318bf7421ee97cbd36cbfa0ffe0243fca
Paul Clough <pclough@miami.edu>
2025-10-22 12:00:57 -0400

    Show all (not just top-level) search results
    Currently, when a user searches a repository/distinctive collection from the left-hand search box only the top-level (collection) results are displayed. The user should see results from any level of the record.

--- apps/qubit/modules/repository/templates/_holdingsInstitution.php
+++ apps/qubit/modules/repository/templates/_holdingsInstitution.php
@@ -26,6 +26,7 @@
 
       <form class="sidebar-search" role="search" aria-label="<?php echo __(sfConfig::get('app_ui_label_institutionSearchHoldings')); ?>" action="<?php echo url_for(['module' => 'informationobject', 'action' => 'browse']); ?>">
         <input type="hidden" name="repos" value="<?php echo $resource->id; ?>">
+        <input type="hidden" name="topLod" value="0" />
         <div class="input-prepend input-append">
           <input type="text" name="query" aria-label="<?php echo __('Search'); ?>" value="<?php echo $sf_request->query; ?>" placeholder="<?php echo __('Search'); ?>">
           <button class="btn" type="submit" aria-label="<?php echo __('Search'); ?>">
