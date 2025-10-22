73766470206901fe68be0a02541e33a6d0180de2
Paul Clough <pclough@miami.edu>
2025-10-22 11:53:14 -0400

    Reconfigure "Physical storage location" display
    Make changes to the "Physical storage location" layout so it will more closely match the main column layout.

--- apps/qubit/modules/physicalobject/templates/_contextMenu.php
+++ apps/qubit/modules/physicalobject/templates/_contextMenu.php
@@ -1,27 +1,27 @@
 <section id="physical-objects">
 
-  <h4><?php echo sfConfig::get('app_ui_label_physicalobject'); ?></h4>
+  <h2><?php echo sfConfig::get( 'app_ui_label_physicalobject' ) ?></h2>
 
-  <div class="content">
-    <ul>
-
-      <?php foreach ($physicalObjects as $item) { ?>
-        <li>
-
-          <?php if (isset($item->type)) { ?>
-            <?php echo render_value_inline($item->type); ?>:
-          <?php } ?>
-
-          <?php echo link_to_if(QubitAcl::check($resource, 'update'), render_title($item), [$item, 'module' => 'physicalobject']); ?>
-
-          <?php if (isset($item->location) && $sf_user->isAuthenticated()) { ?>
-            - <?php echo render_value_inline($item->getLocation(['cultureFallback' => 'true'])); ?>
-          <?php } ?>
-
-        </li>
-      <?php } ?>
-
-    </ul>
-  </div>
+  <?php foreach ( $physicalObjects as $item ): ?>
+    <div class="field">
+      <h3>
+        <?php if ( $resource != $resource->getCollectionRoot() ): ?>
+          <?php if ( isset( $item->type )): ?>
+            <?php echo $item->type ?>:
+          <?php endif; ?>
+        <?php endif; ?>
+        <?php if ( isset( $item->location )): ?>
+          <?php echo render_title( $item ) ?>
+        <?php endif; ?>
+      </h3>
+      <div>
+        <?php if ( isset( $item->location )): ?>
+          <?php echo $item->getLocation( array( 'cultureFallback' => 'true' )) ?>
+        <?php else: ?>
+          <?php echo render_title( $item ) ?>
+        <?php endif; ?>
+      </div>
+    </div>
+  <?php endforeach; ?>
 
 </section>
