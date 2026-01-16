11941f83613a28501944660add8927ec7f7d6b86
Paul Clough <pclough@miami.edu>
2026-01-16 15:45:56 -0500

    Revert "Merge pull request #3 from paclough/copilot/fix-display-issues-umlplugin"
    This reverts commit 68ccc114631b472a19ddb527baf85f2d839c2826, reversing
    changes made to 628ccdd7545e6880adfdf9fb864ef1402cd63a6e.

--- plugins/umlDominionB5Plugin/templates/_header.php
+++ plugins/umlDominionB5Plugin/templates/_header.php
@@ -19,9 +19,7 @@
     <?php if (sfConfig::get('app_toggleLogo') || sfConfig::get('app_toggleTitle')) { ?>
       <a class="navbar-brand d-flex flex-wrap flex-lg-nowrap align-items-center py-0 me-0" href="<?php echo url_for('@homepage'); ?>" title="<?php echo __('Home'); ?>" rel="home">
         <?php if (sfConfig::get('app_toggleLogo')) { ?>
-          <div id="logo">
-            <?php echo image_tag('/plugins/arDominionB5Plugin/images/logo', ['alt' => __('AtoM logo'), 'class' => 'd-inline-block my-2 me-3', 'height' => '35']); ?>
-          </div>
+          <?php echo image_tag('/plugins/arDominionB5Plugin/images/logo', ['alt' => __('AtoM logo'), 'class' => 'd-inline-block my-2 me-3', 'height' => '35']); ?>
         <?php } ?>
         <?php if (sfConfig::get('app_toggleTitle') && !empty(sfConfig::get('app_siteTitle'))) { ?>
           <span class="text-wrap my-1 me-3"><?php echo esc_specialchars(sfConfig::get('app_siteTitle')); ?></span>
